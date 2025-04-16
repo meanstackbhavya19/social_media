86d793b47073cd7c3064041fb403fd434edbeef6
Bhavya Verma <bhavyaverma777@Bhavyas-MacBook-Air.local>
2025-04-16 15:05:09 +0530

    your code

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app=express()
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+const PORT=3000
+app.listen(3000,()=>{
+    console.log("app is running")
+})
